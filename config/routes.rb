cd2d2d6d40fd20a8c289288c918255a5b194c84c
Puru Dahal <pdahal@me.com>
2014-03-20 11:16:05 -0500

    Links to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Inventory::Application.routes.draw do
   devise_for :users
   resources :clothes
   root 'welcome#index'
+  get 'clothes/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
